4cc02a8fd79d66e198ea2eebb779744d3c8898ca
Roman DILMI <roman.dilmi@orange.fr>
2019-10-11 16:14:25 +0200

    001 : Add page for blog

---
+++ pages/blog.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+
+import React from 'react';
+
+
+class Blog extends React.Component {
+
+constructor(props) {
+  super(props);
+}
+
+
+  render() {
+
+    return (
+      <Layout>
+        <h1 data-testid="page-title">Blog</h1>
+        <p data-testid="text">Blog page</p>
+
+      </Layout>
+    );
+  }
+}
+
+export default Blog;
