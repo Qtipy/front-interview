f68b9f3b305b309811e63f4ea8e61a2f9317f618
Roman DILMI <roman.dilmi@orange.fr>
2019-10-11 16:22:51 +0200

    003 : Creating component for blog

---
+++ components/BlogScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class BlogScreen extends React.Component {
+
+constructor(props) {
+  super(props);
+}
+
+
+  render() {
+
+    return (
+        <h1 data-testid="page-title">BlogScreen</h1>
+    );
+  }
+}
+
+export default BlogScreen;
--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Layout from '../components/Layout';
 
 import React from 'react';
+import BlogScreen from '../components/BlogScreen';
 
 
 class Blog extends React.Component {
@@ -17,6 +18,8 @@ constructor(props) {
       <Layout>
         <h1 data-testid="page-title">Blog</h1>
         <p data-testid="text">Blog page</p>
+
+        <BlogScreen/>
 
       </Layout>
     );
